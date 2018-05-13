87e253abe63f1872771659fde37e0b4eca28a7da
Andy Miguel <andymiguel04@gmail.com>
2018-05-12 18:57:23 -0700

    added deck.go

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	cards := []string{"Ace of Spades", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
